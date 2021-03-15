6512473e3534afcca3a6fdb2542a9a66dbfe3553
MatheLover <rockongoodtimes@yahoo.com>
2021-03-15 08:47:38 +0800

    Choropleth Map main part done

--- infectious_disease_site/malaria/views.py
+++ infectious_disease_site/malaria/views.py
@@ -5,6 +5,12 @@ from bokeh.embed import components
 from bokeh.plotting import figure
 import pandas as pd
 import folium
+import branca.colormap as cm
+import geopandas as gpd
+import numpy as np
+
+from datetime import datetime, timedelta
+
 
 
 # Create your views here.
@@ -153,5 +159,263 @@ def malaria_cumulative_stat_view(request):
     return render(request, 'malaria/malaria_cumulative_stat.html')
 
 def malaria_cumulative_stat_map_view(request):
+    obtained_feature = request.GET.get("Feature")
+    if obtained_feature == "Cases":
+        slider_name_list = []
+        slider_case_list = []
+        slider_lat_list = []
+        slider_lon_list = []
+        slider_year_list = []
+
+        for m in Malaria.objects.all():
+            slider_lat_list.append(m.Latitude)
+            slider_lon_list.append(m.Longitude)
+            slider_name_list.append(m.Country)
+            slider_case_list.append(m.Cases)
+            year = str(m.Year)
+            if year != '2010':
+                year = year + '/12/31'
+            slider_year_list.append(year)
+
+        country_case = zip(slider_name_list, slider_case_list, slider_year_list)
+        slider_zipped_country_case = list(country_case)
+        df_slider = pd.DataFrame(data=slider_zipped_country_case, columns=['Country', 'Cases', 'Year'])
+        df_slider = df_slider[df_slider.Cases != 0]
+
+        # sorting
+        sorted_df = df_slider.sort_values(['Country',
+                                           'Year']).reset_index(drop=True)
+
+        # Combine data with the file
+        country = gpd.read_file("/Users/benchiang/Desktop/countries.geojson")
+        country = country.rename(columns={'ADMIN': 'Country'})
+        combined_df = sorted_df.merge(country, on='Country')
+        # print(combined_df)
+
+        # Use Log to plot the cases
+        combined_df['log_cases'] = np.log10(combined_df['Cases'])
+        combined_df = combined_df[['Country', 'log_cases', 'Year', 'geometry']]
+        # print(combined_df)
+
+        combined_df['Year'] = pd.to_datetime(combined_df['Year']).astype(int) / 10 ** 9
+        combined_df['Year'] = combined_df['Year'].astype(int).astype(str)
+
+        # Construct color map
+        max_color = max(combined_df['log_cases'])
+        min_color = min(combined_df['log_cases'])
+        color_map = cm.linear.YlOrRd_09.scale(min_color, max_color)
+        combined_df['color'] = combined_df['log_cases'].map(color_map)
+        # print(combined_df)
+
+        # Construct style dictionary
+        unique_country_list = combined_df['Country'].unique().tolist()
+        ctry_index = range(len(unique_country_list))
+
+        style_dic = {}
+        for j in ctry_index:
+            ctry = unique_country_list[j]
+            country = combined_df[combined_df['Country'] == ctry]
+            in_dic = {
+
+            }
+            for _, r in country.iterrows():
+                in_dic[r['Year']] = {'color': r['color'], 'opacity': 0.8}
+            style_dic[str(j)] = in_dic
+
+        # Make a dataframe containing each country
+        specific_ctry = combined_df[['geometry']]
+        ctry_gdf = gpd.GeoDataFrame(specific_ctry)
+        ctry_gdf = ctry_gdf.drop_duplicates().reset_index()
+
+        # Create a slider map
+        from folium.plugins import TimeSliderChoropleth
+        m = folium.Map(min_zoom=2, max_bounds=True, tiles='cartodbpositron')
+
+        # Plot Slider Map
+        _ = TimeSliderChoropleth(
+            data=ctry_gdf.to_json(),
+            styledict=style_dic,
+        ).add_to(m)
+        _ = color_map.add_to(m)
+        color_map.caption = "Log number of malaria cases"
+
+
+        m.save("malaria/malaria_cumulative_stat_map.html")
+        m = m._repr_html_()
+        context = {
+            'm': m
+        }
+
+
+        return render(request, 'malaria/malaria_cumulative_stat_map.html', context)
+
+    elif obtained_feature == "Population_at_risk":
+        slider_name_list = []
+        slider_pop_list = []
+        slider_lat_list = []
+        slider_lon_list = []
+        slider_year_list = []
+
+        for m in Malaria.objects.all():
+            slider_lat_list.append(m.Latitude)
+            slider_lon_list.append(m.Longitude)
+            slider_name_list.append(m.Country)
+            slider_pop_list.append(m.Population_at_risk)
+            year = str(m.Year)
+            if year != '2010':
+                year = year + '/12/31'
+            slider_year_list.append(year)
+
+        country_case = zip(slider_name_list, slider_pop_list, slider_year_list)
+        slider_zipped_country_case = list(country_case)
+        df_slider = pd.DataFrame(data=slider_zipped_country_case, columns=['Country', 'Population_at_risk', 'Year'])
+        df_slider = df_slider[df_slider.Population_at_risk != 0]
+
+        # sorting
+        sorted_df = df_slider.sort_values(['Country',
+                                           'Year']).reset_index(drop=True)
+
+        # Combine data with the file
+        country = gpd.read_file("/Users/benchiang/Desktop/countries.geojson")
+        country = country.rename(columns={'ADMIN': 'Country'})
+        combined_df = sorted_df.merge(country, on='Country')
+        # print(combined_df)
+
+        # Use Log to plot the cases
+        combined_df['log_pop'] = np.log10(combined_df['Population_at_risk'])
+        combined_df = combined_df[['Country', 'log_pop', 'Year', 'geometry']]
+        # print(combined_df)
+
+        combined_df['Year'] = pd.to_datetime(combined_df['Year']).astype(int) / 10 ** 9
+        combined_df['Year'] = combined_df['Year'].astype(int).astype(str)
+
+        # Construct color map
+        max_color = max(combined_df['log_pop'])
+        min_color = min(combined_df['log_pop'])
+        color_map = cm.linear.YlOrRd_09.scale(min_color, max_color)
+        combined_df['color'] = combined_df['log_pop'].map(color_map)
+        # print(combined_df)
+
+        # Construct style dictionary
+        unique_country_list = combined_df['Country'].unique().tolist()
+        ctry_index = range(len(unique_country_list))
+
+        style_dic = {}
+        for j in ctry_index:
+            ctry = unique_country_list[j]
+            country = combined_df[combined_df['Country'] == ctry]
+            in_dic = {
+
+            }
+            for _, r in country.iterrows():
+                in_dic[r['Year']] = {'color': r['color'], 'opacity': 0.8}
+            style_dic[str(j)] = in_dic
+
+        # Make a dataframe containing each country
+        specific_ctry = combined_df[['geometry']]
+        ctry_gdf = gpd.GeoDataFrame(specific_ctry)
+        ctry_gdf = ctry_gdf.drop_duplicates().reset_index()
+
+        # Create a slider map
+        from folium.plugins import TimeSliderChoropleth
+        m = folium.Map(min_zoom=2, max_bounds=True, tiles='cartodbpositron')
+        _ = TimeSliderChoropleth(
+            data=ctry_gdf.to_json(),
+            styledict=style_dic,
+        ).add_to(m)
+        _ = color_map.add_to(m)
+        color_map.caption = "Log Number of Population at Risk"
+
+        m.save("malaria/malaria_cumulative_stat_map.html")
+        m = m._repr_html_()
+        context = {
+            'm': m
+        }
+        return render(request, 'malaria/malaria_cumulative_stat_map.html',context)
+
+    else:
+        slider_name_list = []
+        slider_death_list = []
+        slider_lat_list = []
+        slider_lon_list = []
+        slider_year_list = []
+
+        for m in Malaria.objects.all():
+            slider_lat_list.append(m.Latitude)
+            slider_lon_list.append(m.Longitude)
+            slider_name_list.append(m.Country)
+            slider_death_list.append(m.Deaths)
+            year = str(m.Year)
+            if year != '2010':
+                year = year + '/12/31'
+            slider_year_list.append(year)
+
+        country_case = zip(slider_name_list, slider_death_list, slider_year_list)
+        slider_zipped_country_case = list(country_case)
+        df_slider = pd.DataFrame(data=slider_zipped_country_case, columns=['Country', 'Deaths', 'Year'])
+        df_slider = df_slider[df_slider.Deaths != 0]
+
+        # sorting
+        sorted_df = df_slider.sort_values(['Country',
+                                           'Year']).reset_index(drop=True)
+
+        # Combine data with the file
+        country = gpd.read_file("/Users/benchiang/Desktop/countries.geojson")
+        country = country.rename(columns={'ADMIN': 'Country'})
+        combined_df = sorted_df.merge(country, on='Country')
+        # print(combined_df)
+
+        # Use Log to plot the cases
+        #combined_df['log_pop'] = np.log10(combined_df['Population_at_risk'])
+        combined_df = combined_df[['Country', 'Deaths', 'Year', 'geometry']]
+        # print(combined_df)
+
+        combined_df['Year'] = pd.to_datetime(combined_df['Year']).astype(int) / 10 ** 9
+        combined_df['Year'] = combined_df['Year'].astype(int).astype(str)
+
+        # Construct color map
+        max_color = max(combined_df['Deaths'])
+        min_color = min(combined_df['Deaths'])
+        color_map = cm.linear.YlOrRd_09.scale(min_color, max_color)
+        combined_df['color'] = combined_df['Deaths'].map(color_map)
+        # print(combined_df)
+
+        # Construct style dictionary
+        unique_country_list = combined_df['Country'].unique().tolist()
+        ctry_index = range(len(unique_country_list))
+
+        style_dic = {}
+        for j in ctry_index:
+            ctry = unique_country_list[j]
+            country = combined_df[combined_df['Country'] == ctry]
+            in_dic = {
+
+            }
+            for _, r in country.iterrows():
+                in_dic[r['Year']] = {'color': r['color'], 'opacity': 0.8}
+            style_dic[str(j)] = in_dic
+
+        # Make a dataframe containing each country
+        specific_ctry = combined_df[['geometry']]
+        ctry_gdf = gpd.GeoDataFrame(specific_ctry)
+        ctry_gdf = ctry_gdf.drop_duplicates().reset_index()
+
+        # Create a slider map
+        from folium.plugins import TimeSliderChoropleth
+        m = folium.Map(min_zoom=2, max_bounds=True, tiles='cartodbpositron')
+        _ = TimeSliderChoropleth(
+            data=ctry_gdf.to_json(),
+            styledict=style_dic,
+        ).add_to(m)
+        _ = color_map.add_to(m)
+        color_map.caption = "Number of Deaths"
+
+        m.save("malaria/malaria_cumulative_stat_map.html")
+        m = m._repr_html_()
+        context = {
+            'm': m
+        }
+        return render(request, 'malaria/malaria_cumulative_stat_map.html', context)
+
     return render(request, 'malaria/malaria_cumulative_stat_map.html')
 
